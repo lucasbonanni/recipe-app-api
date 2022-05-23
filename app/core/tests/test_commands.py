87f130502c54fc49525919f000764344531eb38a
Lucas Bonanni <bonanni.lucas.esteban@gmail.com>
2022-05-23 06:02:33 -0300

    Fix linting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -8,30 +8,31 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch("core.management.commands.wait_for_db.Command.check")
 class CommandTests(SimpleTestCase):
-    """ Test commands."""
+    """Test commands."""
 
     def test_wait_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
 
-        call_command('wait_for_db')
+        call_command("wait_for_db")
 
-        patched_check.assert_called_once_with(databases=['default'])
+        patched_check.assert_called_once_with(databases=["default"])
 
-    @patch('time.sleep')
+    @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
 
-        'Sequential mock'
-        'Whe are mocking an exception an configuring to Pyscopg2Error occur two times thats why has *2' # noqa
-        'Whe are mocking an exception an configuring to OperationalError occur three times thats why has *3' # noqa
-        'And if a boolean is passed return a boolean at the last call'
-        patched_check.side_effect = [Psycopg2Error] * 2 +\
-            [OperationalError] * 3 + [True]
+        "Sequential mock"
+        "Whe are mocking an exception an configuring to Pyscopg2Error occur two times thats why has *2"  # noqa
+        "Whe are mocking an exception an configuring to OperationalError occur three times thats why has *3"  # noqa
+        "And if a boolean is passed return a boolean at the last call"
+        patched_check.side_effect = (
+            [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        )
 
-        call_command('wait_for_db')
+        call_command("wait_for_db")
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
+        patched_check.assert_called_with(databases=["default"])
