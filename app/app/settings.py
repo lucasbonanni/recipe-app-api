8c46f28f44cdfa227a4bef95642fa7e537b7f0e7
Lucas Bonanni <bonanni.lucas.esteban@gmail.com>
2022-06-28 08:27:29 -0300

    Add swagger to django and rest module

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_spectacular',
     'core',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
