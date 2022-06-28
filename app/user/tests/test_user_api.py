ff476d96f035916c7c71ed815c91709ed5fc0292
Lucas Bonanni <bonanni.lucas.esteban@gmail.com>
2022-06-28 19:03:40 -0300

    Add the user serializer and the user view to create a new user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -47,7 +47,7 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUESTS)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
         """Test an error is returned if password less than 5 chars."""
