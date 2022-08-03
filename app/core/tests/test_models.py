41da137e4d47b72a42eb530129cb6cdc3fdee8e2
Lucas Bonanni <bonanni.lucas.esteban@gmail.com>
2022-08-03 07:00:23 -0300

    Add Tag views and serializers

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,11 +7,12 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
-def create_user(email='user@example.com', password='testpass123'):
-    """Create and return a new user."""
-    return get_user_model().objects.create_user(email,password
 
-                                                )
+def create_user(email="user@example.com", password="testpass123"):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """TEst models."""
 
@@ -26,25 +27,25 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
-            ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
-            ['test4@example.COM', 'test4@example.com'],
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.com", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         """TEst that creating a user without an email raises a ValueError"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'test123')
+            get_user_model().objects.create_user("", "test123")
 
     def test_create_superuser(self):
         """Test creating a superuser"""
         user = get_user_model().objects.create_superuser(
-            'test@example.com',
-            'test123',
+            "test@example.com",
+            "test123",
         )
 
         self.assertTrue(user.is_superuser)
@@ -52,16 +53,13 @@ class ModelTests(TestCase):
 
     def test_create_recipe(self):
         """Test creating a recipe is successful."""
-        user = get_user_model().objects.create_user(
-            'test@example.com',
-            'testpass123'
-        )
+        user = get_user_model().objects.create_user("test@example.com", "testpass123")
         recipe = models.Recipe.objects.create(
             user=user,
-            title='Sample recipe name',
+            title="Sample recipe name",
             time_minutes=5,
-            price=Decimal('5.50'),
-            description='Sample recipe description'
+            price=Decimal("5.50"),
+            description="Sample recipe description",
         )
 
         self.assertEqual(str(recipe), recipe.title)
@@ -69,6 +67,6 @@ class ModelTests(TestCase):
     def test_create_tag(self):
         """Test creating a tag is successful."""
         user = create_user()
-        tag = models.Tag.objects.create(user=user, name='Tag1')
+        tag = models.Tag.objects.create(user=user, name="Tag1")
 
-        self.assertEqual(str(tag),tag.name)
+        self.assertEqual(str(tag), tag.name)
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,9 +1,10 @@
 """
 Serializers for recipe APIs
 """
+
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -11,12 +12,21 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
-        read_only_fields = ['id']
+        fields = ["id", "title", "time_minutes", "price", "link"]
+        read_only_fields = ["id"]
 
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ["description"]
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ["id", "name"]
+        read_only_fields = ["id"]
