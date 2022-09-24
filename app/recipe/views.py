756837806c8dd89c760eb2708ab4c079ceb9b620
Lucas Bonanni <bonanni.lucas.esteban@gmail.com>
2022-09-24 17:05:24 -0300

    Add filtering by tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,12 @@
 """
 Views for the recipe APIs
 """
+from drf_spectacular.utils import(
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
 from rest_framework import (
     viewsets,
     mixins,
@@ -23,9 +29,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def _params_to_ints(self,qs):
+        """Convert a list of string to integers."""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._prams_to_ints(ingredients)
+            queryset = queryset.filter(ingredient__id__in=ingredient_ids)
+        
+        return self.queryset.filter(user=self.request.user).order_by('-id').distinct()
+
     def get_serializer_class(self):
         """Return the serializer class for request."""
         if self.action == 'list':
